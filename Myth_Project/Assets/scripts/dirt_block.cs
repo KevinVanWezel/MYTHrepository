7870ab7400b63c677554087f2a07ebdce3a09910
KevinVanWezel <21973@ma-web.nl>
2018-03-16 13:39:31 +0100

    push

---
+++ Myth_Project/Assets/scripts/dirt_block.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dirt_block : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GameObject[] pickups = GameObject.FindGameObjectsWithTag("dirt_block");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        print("test");
+        if (other.tag == "Player")
+        {
+
+            Destroy(gameObject);
+
+
+        }
+    }
+}
